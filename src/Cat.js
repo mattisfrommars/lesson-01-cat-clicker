7221aa5e9909951edcc1fe2b12044ebbe541804b
Matt Jewell <m@mattjewell.co.uk>
2016-06-02 06:22:12 +0100

    cat has clicks, TDD style

--- src/Cat.js
+++ src/Cat.js
@@ -1,7 +1,25 @@
-class Cat {
-    get clicks () {
-        return 0;
-    }
+// class definition
+function Cat () {
+    // private properties
+    var clicks = 0;
+    Object.defineProperties(this, {
+        "clicks": {
+            enumerable: true,
+            get () {
+                return clicks;
+            }
+        },
+        "increment": {
+            value: function () {
+                clicks += 1;
+                return this;
+            }
+        }
+    });
+}
+// factory methods e.g. createCatWithFooBarBaz(foo,bar,baz)
+function createCat () {
+    return new Cat();
 }
 
-export default Cat;
+export {createCat}
--- test/Cat.js
+++ test/Cat.js
@@ -1,15 +1,40 @@
-import Cat from "../src/Cat";
+import {createCat as createCat} from "../src/Cat";
 import {describe, it, beforeEach} from "mocha";
 import assert from "assert";
 
 describe("A Cat", function () {
     var cat;
     beforeEach(function () {
-        cat = new Cat();
+        cat = createCat();
     });
     it("has clicks", function () {
         var expected = 0;
         var actual = cat.clicks;
-        assert.equal(expected, actual);
+        assert.equal(actual, expected, "cat should have 0 clicks");
+    });
+    it("cannot set clicks directly", function () {
+        try {
+            cat.clicks = 1;
+        } catch (ignored) {}
+        var expected = 0;
+        var actual = cat.clicks;
+        assert.equal(actual, expected, "cat should still have 0 clicks");
+    });
+    it("can increment the number of clicks", function () {
+        cat.increment();
+        var expected = 1;
+        var actual = cat.clicks;
+        assert.equal(actual, expected, "cat should now have 1 click");
+        cat.increment();
+        cat.increment();
+        cat.increment();
+        expected = 4;
+        actual = cat.clicks;
+        assert.equal(actual, expected, "cat should now have 4 clicks");
+    });
+    it("conforms to a fluent interface", function () {
+        var expected = 1;
+        var actual = cat.increment().clicks;
+        assert.equal(actual, expected, "cat should have 1 click and shouldn't have thrown an exception");
     });
 });
